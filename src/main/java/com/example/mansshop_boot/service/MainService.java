b8b691cb08f35ac48476159da5bee097d8bab975
Youndae <namyoundae@gmail.com>
2024-07-18 10:25:50 +0900

    24/07/18

--- src/main/java/com/example/mansshop_boot/service/MainService.java
+++ src/main/java/com/example/mansshop_boot/service/MainService.java
@@ -3,7 +3,10 @@ package com.example.mansshop_boot.service;
 import com.example.mansshop_boot.domain.dto.main.MainListResponseDTO;
 import com.example.mansshop_boot.domain.dto.pageable.MemberPageDTO;
 import com.example.mansshop_boot.domain.dto.response.serviceResponse.PagingListDTO;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.ResponseEntity;
 
+import java.net.URL;
 import java.security.Principal;
 import java.util.List;
 
@@ -12,4 +15,10 @@ public interface MainService {
     List<MainListResponseDTO> getBestAndNewList(MemberPageDTO pageDTO, Principal principal);
 
     PagingListDTO<MainListResponseDTO> getClassificationAndSearchList(MemberPageDTO pageDTO, Principal principal);
+
+    /*URL getSignedUrl(String imageName);*/
+
+    ResponseEntity<InputStreamResource> getSignedUrl(String imageName);
+
+
 }
